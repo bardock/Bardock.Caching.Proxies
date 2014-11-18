4679669490a43e4b0caa6509881d9e2f7664307a
fsanchez-originsoftware <fsanchez@originsoftware.com>
2014-11-18 11:02:08 -0300

    unit tests

---
+++ tests/Bardock.Caching.Proxies.Tests/CacheMock.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using Sixeyed.Caching;
+
+namespace Bardock.Caching.Proxies.Tests
+{
+    public class CacheMock : ICache
+    {
+        private Dictionary<string, object> _storage;
+
+        public CacheMock()
+        {
+            Reset();
+        }
+
+        public void Reset()
+        {
+            _storage = new Dictionary<string, object>();
+        }
+
+        public CacheType CacheType
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public virtual bool Exists(string key)
+        {
+            return _storage.ContainsKey(key);
+        }
+
+        public virtual T Get<T>(string key, Sixeyed.Caching.Serialization.SerializationFormat serializationFormat = Sixeyed.Caching.Serialization.SerializationFormat.Null)
+        {
+            return (T)Get(typeof(T), key, serializationFormat);
+        }
+
+        public virtual object Get(Type type, string key, Sixeyed.Caching.Serialization.SerializationFormat serializationFormat = Sixeyed.Caching.Serialization.SerializationFormat.Null)
+        {
+            if (!Exists(key))
+                throw new CacheKeyNotFoundException();
+            return _storage[key];
+        }
+
+        public virtual Dictionary<string, T> GetAll<T>(Sixeyed.Caching.Serialization.SerializationFormat serializationFormat = Sixeyed.Caching.Serialization.SerializationFormat.Null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual Dictionary<string, object> GetAll(Type type, Sixeyed.Caching.Serialization.SerializationFormat serializationFormat = Sixeyed.Caching.Serialization.SerializationFormat.Null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void Initialise()
+        {
+        }
+
+        public virtual void Remove(string key)
+        {
+            _storage.Remove(key);
+        }
+
+        public virtual void RemoveAll(string keyPrefix)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void RemoveAll()
+        {
+            Reset();
+        }
+
+        public virtual Sixeyed.Caching.Serialization.Serializer Serializer
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public virtual void Set(string key, object value, TimeSpan validFor, Sixeyed.Caching.Serialization.SerializationFormat serializationFormat = Sixeyed.Caching.Serialization.SerializationFormat.Null)
+        {
+            _storage[key] = value;
+        }
+
+        public virtual void Set(string key, object value, DateTime expiresAt, Sixeyed.Caching.Serialization.SerializationFormat serializationFormat = Sixeyed.Caching.Serialization.SerializationFormat.Null)
+        {
+            _storage[key] = value;
+        }
+
+        public virtual void Set(string key, object value, Sixeyed.Caching.Serialization.SerializationFormat serializationFormat = Sixeyed.Caching.Serialization.SerializationFormat.Null)
+        {
+            _storage[key] = value;
+        }
+    }
+}
---
+++ tests/Bardock.Caching.Proxies.Tests/DeferredCacheProxyTest.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Threading.Tasks;
+using Moq;
+using Sixeyed.Caching.Serialization;
+using Xunit;
+
+namespace Bardock.Caching.Proxies.Tests
+{
+    public class DeferredCacheProxyTest
+    {
+        private const string KEY = "KEY";
+
+        [Fact]
+        public void GetData_DefaultExpiration()
+        {
+            var cache = new Mock<CacheMock>() { CallBase = true };
+
+            var proxy = new DeferredCacheProxy<DateTime>(cache.Object, KEY);
+
+            var value = new DateTime(2014, 1, 2, 3, 4, 5);
+
+            var v1 = proxy.GetData(() => value);
+            var v2 = proxy.GetData(() => DateTime.Now);
+            var v3 = proxy.GetData(() => DateTime.Now);
+
+            Assert.Equal(value, v1);
+            Assert.Equal(v1, v2);
+            Assert.Equal(v2, v3);
+
+            cache.Verify(c => c.Set(KEY, value, DeferredCacheProxy<DateTime>.EXPIRATION_DEFAULT, SerializationFormat.Null), Times.Once);
+            cache.Verify(c => c.Get<DateTime>(KEY, SerializationFormat.Null), Times.Exactly(3));
+        }
+
+        [Fact]
+        public void GetData_CustomExpiration()
+        {
+            var cache = new Mock<CacheMock>() { CallBase = true };
+
+            var expiration = TimeSpan.FromMinutes(1);
+            var proxy = new DeferredCacheProxy<DateTime>(cache.Object, KEY, expiration);
+
+            var value = new DateTime(2014, 1, 2, 3, 4, 5);
+
+            var v1 = proxy.GetData(() => value);
+            var v2 = proxy.GetData(() => DateTime.Now);
+            var v3 = proxy.GetData(() => DateTime.Now);
+
+            Assert.Equal(value, v1);
+            Assert.Equal(v1, v2);
+            Assert.Equal(v2, v3);
+
+            cache.Verify(c => c.Set(KEY, value, expiration, SerializationFormat.Null), Times.Once);
+            cache.Verify(c => c.Get<DateTime>(KEY, SerializationFormat.Null), Times.Exactly(3));
+        }
+
+        [Fact]
+        public void GetData_Multithreaded()
+        {
+            var cache = new Mock<CacheMock>() { CallBase = true };
+
+            var proxy = new DeferredCacheProxy<DateTime>(cache.Object, KEY);
+
+            var value = new DateTime(2014, 1, 2, 3, 4, 5);
+
+            DateTime v1 = default(DateTime);
+            DateTime v2 = default(DateTime);
+            DateTime v3 = default(DateTime);
+            Parallel.Invoke(
+                () => { v1 = proxy.GetData(() => value); },
+                () => { v2 = proxy.GetData(() => value); },
+                () => { v3 = proxy.GetData(() => value); });
+
+            Assert.Equal(value, v1);
+            Assert.Equal(v1, v2);
+            Assert.Equal(v2, v3);
+
+            cache.Verify(c => c.Set(KEY, value, DeferredCacheProxy<DateTime>.EXPIRATION_DEFAULT, SerializationFormat.Null), Times.Once);
+            cache.Verify(c => c.Get<DateTime>(KEY, SerializationFormat.Null), Times.Exactly(3));
+        }
+
+        [Fact]
+        public void GetData_Clear()
+        {
+            var cache = new Mock<CacheMock>() { CallBase = true };
+
+            var proxy = new DeferredCacheProxy<DateTime>(cache.Object, KEY);
+
+            var value = new DateTime(2014, 1, 2, 3, 4, 5);
+            var value2 = new DateTime(2014, 6, 7, 8, 9, 10);
+
+            var v1 = proxy.GetData(() => value);
+            var v2 = proxy.GetData(() => DateTime.Now);
+            proxy.Clear();
+            var v3 = proxy.GetData(() => value2);
+
+            Assert.Equal(value, v1);
+            Assert.Equal(v1, v2);
+            Assert.NotEqual(v2, v3);
+            Assert.Equal(value2, v3);
+
+            cache.Verify(c => c.Set(KEY, value, DeferredCacheProxy<DateTime>.EXPIRATION_DEFAULT, SerializationFormat.Null), Times.Once);
+            cache.Verify(c => c.Set(KEY, value2, DeferredCacheProxy<DateTime>.EXPIRATION_DEFAULT, SerializationFormat.Null), Times.Once);
+            cache.Verify(c => c.Get<DateTime>(KEY, SerializationFormat.Null), Times.Exactly(3));
+        }
+    }
+}
